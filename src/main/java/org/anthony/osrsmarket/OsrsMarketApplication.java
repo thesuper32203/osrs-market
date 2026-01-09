359d487c23e51023356135265412704e7521da7e
Anthony <thesuper32203@gmail.com>
2026-01-08 20:28:44 -0500

    first commit

---
+++ src/main/java/org/anthony/osrsmarket/OsrsMarketApplication.java
@@ -0,0 +1,13 @@
+package org.anthony.osrsmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrsMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsrsMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/anthony/osrsmarket/OsrsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anthony.osrsmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrsMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
